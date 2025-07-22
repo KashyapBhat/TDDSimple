8fe650abc6a94e9f82e4f68766656766f83ee5e2
Kashyap <bobbikashyap@gmail.com>
2025-07-22 21:30:43 +0530

    when empty or null

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.01
+
+- Empty or null string
+
 ## 1.0.0
 
 - Initial version with dart test lib
--- test/simple_tdd_test.dart
+++ test/simple_tdd_test.dart
@@ -1,7 +1,14 @@
+import 'package:simple_tdd/simple_tdd.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('', () {
-   
+  test('should returns 0 for null string', () {
+    final calculator = StringCalculator();
+    expect(calculator.add(null), 0);
+  });
+
+  test('should returns 0 for empty string', () {
+    final calculator = StringCalculator();
+    expect(calculator.add(''), 0);
   });
 }
